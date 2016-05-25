da15dabe273b3dfda3157ff8c940f27eb8c19125
Nguyen Van Dat <=dat.nv@neo-lab.vn>
2016-05-25 15:39:53 +0700

    version 1

--- console.js
+++ console.js
@@ -1,15 +1,26 @@
 var keyword = prompt('Nhập vào keyword thím ơi:');
-//var total_pll = prompt('Nhập vào tổng pll muốn tạo: ');
-var total_pll = 10;
+var total_pll = prompt('Nhập vào tổng pll muốn tạo: ');
+var total_videos = prompt('Nhập vào ID video muốn chèn, cách nhau bằng dấu , : ');
+//var total_videos = 'AVVbZzMhJHU';
+total_videos = total_videos.replace(/ /g,'');
+var all_videos = [];
+all_videos = total_videos.split(',');
+//var total_pll = 2;
 var plls = [];
 var total_times = 0;
 var page_times = 1;
-
+var security_token = '';
+var api_key = 'AIzaSyAsD0yc-a0-Jaaj2YySRMVXmfOwXj9wmAQ';
+var i_video = 0;
+var current_video_id = '';
+total_pll -= 1;
+//.replace(/ /g,'')
 if(keyword == '')
 {
    alert('Keyword trống !');
 } else {
 	search(keyword, page_times);
+	//addVideoToPlaylist('123');
 }
 
 RegExp.prototype.execAll = function(string) {
@@ -26,6 +37,10 @@ RegExp.prototype.execAll = function(string) {
 	}
 	return matches;
 }
+function getStringBetween(string, firstS, lastS)
+{
+	return string.substring(string.lastIndexOf(firstS)+1,string.lastIndexOf(lastS));
+}
 function search(keyword, page = 1)
 {//https://www.youtube.com/results?sp=EgIQAw%253D%253D&search_query=animation+movies&page=1
 	var key_encode = encodeURI(keyword);
@@ -35,34 +50,176 @@ function search(keyword, page = 1)
 	var xhttp = new XMLHttpRequest();
 	  xhttp.onreadystatechange = function() {
 	    if (xhttp.readyState == 4 && xhttp.status == 200) {
-	      filterAllPlaylist(xhttp.responseText);
-	      total_times += 1;
-	    }
-	  };
-	  xhttp.open("GET", url, true);
-	  xhttp.send();
+	    		security_token = 
+				filterAllPlaylist(xhttp.responseText);
+			total_times += 1;
+		}
+	};
+	xhttp.open("GET", url, true);
+	xhttp.send();
 }
 
 function filterAllPlaylist(page_string)
 {
 	var content_string = '';
-	content_string = page_string.substring(page_string.lastIndexOf('class="item-section">')+1,page_string.lastIndexOf('class="yt-uix-pager'));
-	var regex = /&amp;list=([A-Za-z0-9]{30,40})/g;
+	content_string = getStringBetween(page_string, 'class="item-section">','class="yt-uix-pager');
+	var regex = /&amp;list=([A-Za-z0-9-]{30,40})/g;
 	match = regex.execAll(content_string);
 	for(var i = 0; i < match.length; i++)
 	{
+		if(plls.length > total_pll) break;
 		if(plls.indexOf(match[i][1]) == -1)
 		{
 			plls.push(match[i][1]);
 		}
-		if(plls.length >= total_pll) break;
+		
 	}
-	if(total_times >= 4 && (plls.length >= total_pll)) {
-		console.log(plls);
+	//console.log(plls.length); 
+	if(total_times >= 4 || (plls.length >= total_pll)) {
+		console.log('Total playlist has been got: ' + plls.length);
+		var url = 'https://www.youtube.com/playlist?list=' + plls[0];
+		var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (xhttp.readyState == 4 && xhttp.status == 200) {
+					security_token = getStringBetween(xhttp.responseText, 'data-playlist-edit-xsrf-token="', '=" data-');
+					security_token = security_token.replace('ata-playlist-edit-xsrf-token="', '');
+					security_token += '%3D';
+					autoCreate();
+				}
+			};
+		xhttp.open("GET", url, true);
+		xhttp.send();
+		
 	} else {
 		//console.log(plls.length);
 		page_times += 1;
 		search(keyword, page_times);
 	}
 	
+}
+
+function getPlaylistInfo(id)
+{
+	//https://www.googleapis.com/youtube/v3/playlists?key=AIzaSyAsD0yc-a0-Jaaj2YySRMVXmfOwXj9wmAQ&id=PLZnmJWnTfXn4DdfN3bUzUT0SmYhqkzSqX&part=id,snippet
+	//get data
+	var url = 'https://www.googleapis.com/youtube/v3/playlists?key=' + api_key + '&id=' + id + '&part=id,snippet';
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		
+	};
+	xhttp.open("GET", url, false);
+	xhttp.send();
+	if (xhttp.readyState == 4 && xhttp.status == 200) {
+		var arr = JSON.parse(xhttp.responseText);
+		var res = [];
+		try {
+			res['title'] = arr['items'][0]['snippet']['title'];
+			res['des'] = arr['items'][0]['snippet']['description'];
+		} catch(e) {}
+		return res;
+	} else return [];
+}
+
+function sendRq(postVar)
+{
+	var url = 'https://www.youtube.com/playlist_ajax?action_create_playlist=1';
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		
+	};
+	xhttp.open("POST", url, false);
+	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+	xhttp.send(postVar);
+	if (xhttp.readyState == 4 && xhttp.status == 200) {
+		return (xhttp.responseText);
+	}
+}
+
+function sendPostRq(url, postVar)
+{
+	//var url = 'https://www.youtube.com/playlist_ajax?action_create_playlist=1';
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		
+	};
+	xhttp.open("POST", url, false);
+	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+	xhttp.send(postVar);
+	if (xhttp.readyState == 4 && xhttp.status == 200) {
+		return (xhttp.responseText);
+	}
+}
+function sendGetRq(url)
+{
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		
+	};
+	xhttp.open("POST", url, false);
+	xhttp.send();
+	if (xhttp.readyState == 4 && xhttp.status == 200) {
+		return (xhttp.responseText);
+	}
+}
+
+function autoCreate()
+{
+	var url = '';
+	var postVar = '';
+	var page = '';
+	var tmp = [];
+	var plid = '';
+	for(var i = 0; i < plls.length; i++)
+	{
+		tmp = getPlaylistInfo(plls[i]);
+		var postVar = 'video_ids&source_playlist_id='+plls[i]+'&n='+encodeURI(tmp["title"])+'&p=public&session_token='+security_token;
+		page = sendRq(postVar);
+		try {
+			pJson = JSON.parse(page);
+			plid = pJson['result']['playlistId'];
+			addVideoToPlaylist(plid);
+			moveVideoOnTop(plid);
+			setDescription(plid, tmp['des']);
+			console.log('Done: https://www.youtube.com/playlist?list=' + plid);
+		} catch(e) {}
+		
+		//break;
+	}
+	console.log('Done !!!');
+	return;
+}
+
+function addVideoToPlaylist(plid)
+{
+	if( (i_video+1) > all_videos.length ) {
+		i_video = 0;
+	}
+	videoid = all_videos[i_video];
+	current_video_id = videoid;
+	i_video += 1;
+	var url = 'https://www.youtube.com/playlist_edit_service_ajax?action_add_video=1';
+	var postVar = 'video_id='+videoid+'&playlist_id='+plid+'&session_token='+security_token;
+	var req = sendPostRq(url, postVar);
+	return req;
+}
+
+function moveVideoOnTop(plid)
+{
+	url = 'https://www.youtube.com/playlist?list=' + plid;
+	page_str = sendGetRq(url);
+	//data-set-video-id="
+	var regex = /data-set-video-id=\"([A-F0-9]{15,20})/g;
+	match = regex.execAll(page_str);
+	first_video = match[0][1];
+	last_video = match[match.length-1][1];
+	url = 'https://www.youtube.com/playlist_edit_service_ajax/?action_move_video_before=1';
+	postVar = 'playlist_id='+plid+'&set_video_id='+last_video+'&moved_set_video_id_successor='+first_video+'&session_token='+security_token;
+	str = sendPostRq(url, postVar);
+	//console.log(str);
+}
+function setDescription(plid, desc)
+{
+	url = 'https://www.youtube.com/playlist_edit_service_ajax?action_set_playlist_description=1';
+	postVar = 'playlist_id='+plid+'&playlist_description='+encodeURI(desc)+'&session_token='+security_token;
+	str = sendPostRq(url, postVar);
 }
